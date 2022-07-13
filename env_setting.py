598491f453356cb1f329adfe93fa267faaeb5a46
ntut-xuan <t109590031@ntut.org.tw>
2022-07-13 08:48:28 +0800

    update workflow and test.py

--- env_setting.py
+++ env_setting.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-import sys
+
 
 def set_grub():
     f = open("/etc/default/grub", mode="r")
@@ -16,7 +16,9 @@ def set_grub():
                 temp1.append("cgroup_enable=memory")
             if not "swapaccount=1" in text:
                 temp1.append("swapaccount=1")
-
+            if not "systemd.unified_cgroup_hierarchy=false" in text:
+                temp1.append("systemd.unified_cgroup_hierarchy=false")
+            
             temp[index] = spltext[0] + spltext[1] + " " + " ".join(temp1) + spltext[-1]       
     f.close()
 
@@ -34,24 +36,15 @@ def set_kernel():
     if not subprocess.check_output(['cat', '/sys/kernel/mm/transparent_hugepage/khugepaged/defrag']).decode("utf-8") == "0\n": 
         os.system("echo 'kernel/mm/transparent_hugepage/khugepaged/defrag = 0' >> /etc/sysfs.conf")
 
-def run_save(argv=None):
+def run_save():
     os.system("sysctl -p")
     os.system("update-grub")
-    if len(argv) == None:
-        reboot = input("A computer restart is needed to complete your installation. Do you want to restart now?(Y|n)")
-    else:
-        reboot = argv
-    if reboot == "Y" or reboot == "":
-        os.system("reboot")
+    # reboot = input("A computer restart is needed to complete your installation. Do you want to restart now?(Y|n)")
+    # if reboot == "Y" or reboot == "":
+    os.system("reboot")
 
 
 if __name__ == "__main__":
-    reboot_flag = None
-    if len(sys.argv) > 1:
-        if(sys.argv[1] == "y" or sys.argv[1] == 'Y'):
-            reboot_flag = "Y"
-        else:
-            reboot_flag = "N"
     set_grub()
     set_kernel()
-    run_save(reboot_flag)
+    run_save()
