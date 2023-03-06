41a001f18c65af5b0e0a7428da0a4ecede9f2642
Uriah Xuan <t109590031@ntut.org.tw>
2023-03-06 22:51:53 +0800

    Add Swagger page to describe `/judge` route (#26)
    * Complete the route for swagger page
    
    * Complete the swagger spec and swagger page
    
    * Register the swagger blueprint
    
    * Complete the /judge requestbody and response (judge_result WIP)
    
    * Complete the component of judge_result
    
    * Update swagger-ui-dist to 4.17.0

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass, field
 from typing import Any
 
 from route.route import app_route
+from route.swagger.route import swagger_bp
 from utils.sandbox_util import execute_queueing_task_when_exist_empty_box
 
 from flask import Flask
@@ -14,6 +15,7 @@ from flask import Flask
 def create_app(config_filename = None) -> Flask:
     app = Flask(__name__)
     app.register_blueprint(app_route)
+    app.register_blueprint(swagger_bp)
     
     pop_work_timer = threading.Thread(target=execute_queueing_task_when_exist_empty_box)
     pop_work_timer.daemon = True
---
+++ backend/route/swagger/route.py
@@ -0,0 +1,20 @@
+import json
+from typing import Any
+
+from flask import Blueprint, make_response
+
+swagger_bp = Blueprint("swagger", __name__)
+
+@swagger_bp.route("/swagger_spec")
+def fetch_swagger_spec_route():
+    swagger_spec: str = None
+    with open("./swagger/spec.json") as f:
+        swagger_spec = f.read()
+    swagger_spec_dict: dict[str, Any] = json.loads(swagger_spec)
+    return make_response(swagger_spec_dict)
+
+
+@swagger_bp.route("/api")
+def api_page_route():
+    with open("./swagger/page.html") as f:
+        return f.read()
