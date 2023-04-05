05b3764f632c6b3d486c32518e85b50056ee4549
ntut-xuan <t109590031@ntut.org.tw>
2023-04-05 06:22:09 +0000

    Remove the route of swagger

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,6 @@ from threading import Semaphore
 from api.judge.route import judge_api_bp
 from api.judge.util import execute_queueing_task_when_exist_empty_box
 from api.result.route import result_api_bp
-from api.swagger.route import swagger_api_bp
 from api.system.route import system_api_bp
 from api.test_case.route import test_case_api_bp
 from setting.util import Setting, SettingBuilder
@@ -35,7 +34,6 @@ def create_app(config_mapping: dict[str, str] = None) -> Flask:
     
     app.register_blueprint(judge_api_bp)
     app.register_blueprint(result_api_bp)
-    app.register_blueprint(swagger_api_bp)
     app.register_blueprint(system_api_bp)
     app.register_blueprint(test_case_api_bp)
 
