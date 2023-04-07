343d3b021825efeb229ee2cfa3cd72760795f59e
Royis87 <roy05289113@gmail.com>
2023-04-08 01:23:31 +0800

    Add Swap、ASLR、THP environment check and optimize environment setting

--- backend/env_doctor.sh
+++ backend/env_doctor.sh
@@ -7,9 +7,65 @@ cgroup_check() {
     fi
 }
 
+swap_check() {
+    # If swap is disabled, there is nothing to worry about.
+    local swaps
+    swaps=$(swapon --noheadings)
+    if [ -n "$swaps" ] ; then
+        echo "[  FAIL  ] Swap"
+        exit 1
+    fi
+}
+
+aslr_check() {
+    local val
+    if val=$(cat /proc/sys/kernel/randomize_va_space 2>/dev/null) ; then
+        if [ "$val" -ne 0 ] ; then
+            echo "[  FAIL  ] ASLR"
+            exit 1
+        fi
+    else
+        echo "[  SKIP  ] ASLR"
+    fi
+}
+
+thp_check() {
+    local val
+    if val=$(cat /sys/kernel/mm/transparent_hugepage/enabled 2>/dev/null) ; then
+        case $val in
+            *'[never]'*) ;;
+            *)
+            echo "[  FAIL  ] THP"
+            exit 1
+        esac
+    fi
+    if val=$(cat /sys/kernel/mm/transparent_hugepage/defrag 2>/dev/null) ; then
+        case $val in
+            *'[never]'*) ;;
+            *) 
+            echo "[  FAIL  ] THP"
+            exit 1
+        esac
+    fi
+    if val=$(cat /sys/kernel/mm/transparent_hugepage/khugepaged/defrag 2>/dev/null) ; then
+        if [ "$val" -ne 0 ] ; then
+            echo "[  FAIL  ] THP"
+            exit 1
+        fi
+    fi
+}
+
 cgroup_check memory
 cgroup_check cpuacct
 cgroup_check cpuset
-
 echo "[   OK   ] CGroup v1"
+
+swap_check
+echo "[   OK   ] Swap"
+
+aslr_check
+echo "[   OK   ] ASLR"
+
+thp_check
+echo "[   OK   ] THP"
 exit 0
--- backend/env_setting.py
+++ backend/env_setting.py
@@ -1,7 +1,16 @@
+#!/bin/python3
 import os
 import subprocess
 import sys
+import apt
 
+
+def check_and_install_env():
+    cache = apt.Cache()
+    if not cache['sysfsutils'].is_installed:
+        print("Please install sysfsutils package first!")
+        print("apt-get install -y sysfsutils")
+        exit(1)
 
 def set_grub():
     f = open("/etc/default/grub", mode="r")
@@ -28,42 +37,32 @@ def set_grub():
     f.close()
 
 
-def set_kernel():
-    if (
-        not subprocess.check_output(
-            ["cat", "/proc/sys/kernel/randomize_va_space"]
-        ).decode("utf-8")
-        == "0\n"
-    ):
-        os.system("echo 'kernel.randomize_va_space = 0' >> /etc/sysctl.conf")
-    if (
-        not subprocess.check_output(
-            ["cat", "/sys/kernel/mm/transparent_hugepage/enabled"]
-        ).decode("utf-8")
-        == "always madvise [never]\n"
-    ):
-        os.system(
-            "echo 'kernel/mm/transparent_hugepage/enabled = never' >> /etc/sysfs.conf"
-        )
-    if (
-        not subprocess.check_output(
-            ["cat", "/sys/kernel/mm/transparent_hugepage/defrag"]
-        ).decode("utf-8")
-        == "always defer defer+madvise madvise [never]\n"
-    ):
-        os.system(
-            "echo 'kernel/mm/transparent_hugepage/defrag = never' >> /etc/sysfs.conf"
-        )
-    if (
-        not subprocess.check_output(
-            ["cat", "/sys/kernel/mm/transparent_hugepage/khugepaged/defrag"]
-        ).decode("utf-8")
-        == "0\n"
-    ):
-        os.system(
-            "echo 'kernel/mm/transparent_hugepage/khugepaged/defrag = 0' >> /etc/sysfs.conf"
-        )
+def set_boot_task():
+    if not subprocess.check_output( ["cat", "/proc/sys/kernel/randomize_va_space"] ).decode("utf-8") == "0\n" :
+        os.system("echo 'kernel.randomize_va_space = 0' >> /etc/sysfs.conf")
+    if not subprocess.check_output(["cat", "/sys/kernel/mm/transparent_hugepage/enabled"]).decode("utf-8")== "always madvise [never]\n":
+        os.system("echo 'kernel/mm/transparent_hugepage/enabled = never' >> /etc/sysfs.conf")
+    if not subprocess.check_output(["cat", "/sys/kernel/mm/transparent_hugepage/defrag"]).decode("utf-8")== "always defer defer+madvise madvise [never]\n":
+        os.system("echo 'kernel/mm/transparent_hugepage/defrag = never' >> /etc/sysfs.conf")
+    if not subprocess.check_output(["cat", "/sys/kernel/mm/transparent_hugepage/khugepaged/defrag"]).decode("utf-8")== "0\n":
+        os.system("echo 'kernel/mm/transparent_hugepage/khugepaged/defrag = 0' >> /etc/sysfs.conf")
+    if os.path.exists("/etc/rc.local"):
+        f = open("/etc/rc.local", mode="r")
+        temp = f.readlines()
+        f.close()
+        if not "swapoff -a" in temp:
+            temp.insert(1, "swapoff -a")
+            f = open("/etc/rc.local", mode="w")
+            f.writelines(["#!/bin/bash\n", "swapoff -a\n", "exit 0"])
+            f.close()
+    else:
+        f = open("/etc/rc.local", mode="w")
+        f.writelines(["#!/bin/bash\n", "swapoff -a\n", "exit 0"])
+        f.close()
+    
 
+def set_rc_local_permission():
+    os.chmod("/etc/rc.local", 0o775)
 
 def run_save(reboot=True):
     os.system("sysctl -p")
@@ -81,6 +80,8 @@ if __name__ == "__main__":
             reboot_flag = True
         else:
             reboot_flag = False
+    check_and_install_env()
     set_grub()
-    set_kernel()
+    set_boot_task()
+    set_rc_local_permission()
     run_save(reboot=reboot_flag)
