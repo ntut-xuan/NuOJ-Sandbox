da2ed3646ac9bfd7cc717cf3e89b97dbc02c6df6
ntut-xuan <t109590031@ntut.org.tw>
2023-05-30 07:10:22 +0000

    Migrate to heartbeat function and add tests

---
+++ backend/storage/minio_util.py
@@ -0,0 +1,23 @@
+from os import environ
+from typing import Final
+
+from minio import Minio
+from flask import current_app
+
+from setting.util import Setting
+
+
+def heartbeat() -> bool:
+    setting: Setting = current_app.config["setting"]
+    
+    ACCESS_KEY: Final[str] = environ.get("MINIO_ACCESS_KEY")
+    SECRET_KEY: Final[str] = environ.get("MINIO_SECRET_KEY")
+    ENDPOINT: Final[str] = setting.minio.endpoint
+
+    client = Minio(endpoint=ENDPOINT, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
+
+    try:
+        client.bucket_exists("notexistbucket")
+        return True
+    except:
+        return False
