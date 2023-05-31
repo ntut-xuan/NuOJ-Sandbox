7264dab9c6232efaff1b99a517beb61adb2181f2
Uriah Xuan <t109590031@ntut.org.tw>
2023-05-31 18:54:47 +0800

    v0.3.0 (#80)
    * Connect MinIO Service (#73)
    
    * Add minio enable setting
    
    * Add MinIOConfig for loading the setting
    
    * Initialize the storage when app launch
    
    * Add minio and loguru package
    
    * Disable MinIO
    
    * Add setting in conftest.py
    
    * Add minio image for testing
    
    * Add traceback and fix the connect issue by set secure false
    
    * Fix the setting and enable the minio since we have testing container
    
    * Since connect too much it slow the test speed, remove it
    
    * Migrate to heartbeat function and add tests
    
    * Set the minio config in cg conftest
    
    * Build the minio service
    
    * Build the test container in the same time
    
    * Add the minio-test build phase in disable-cg action
    
    * Add trackback
    
    * Fix the trackback placed in wrong place
    
    * Add env in sandbox-test container
    
    * Complete download function and its tests (#74)
    
    * Complete the download function and testcase
    
    * Fix the error of delete file fixture when bucket is not exists
    
    * Implement the action to fetch the testcase when it missing (#75)
    
    * Add check function to check the test case is in the server
    
    * Implement the prepare function to place the testcase
    
    * Add tests
    
    * Fix the file exists issue when initialize
    
    * Fix scale the size of testcase is wrong (#76)
    
    * Fix the test case size error
    
    * Fix the test_case_size doesn't have default value
    
    * Fix the function error since modified the parameter
    
    * Remove tc_upload route (#77)
    
    * Improve code coverage (#79)
    
    * Fix the fetch unavailabel file should raise assertion error
    
    * Remove get_execute_command for checker since the format is fixed
    
    * Remove the checker execute command since we don't need it
    
    * Add the testcase to validate the source filename and dist filename

---
+++ backend/tests/nocg/storage/test_minio_util.py
@@ -0,0 +1,94 @@
+import os
+from copy import deepcopy
+from pathlib import Path
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+from typing import Final
+
+from flask import Flask
+from minio import Minio
+from pytest import MonkeyPatch, fixture, raises
+
+import storage.minio_util
+from storage.minio_util import download, heartbeat, get_client
+
+
+BUCKET_NAME: Final[str] = "testcase"
+FILE_NAME: Final[str] = "test_file"
+FILE_STRING: Final[str] = "test_string"
+
+
+@fixture
+def storage_client(app: Flask) -> Minio:
+    with app.app_context():
+        return get_client()
+
+
+@fixture
+def place_the_file(app: Flask, storage_client: Minio) -> None:
+    _delete_the_file(app, storage_client)
+    _upload_the_file(app, storage_client)
+
+    yield
+
+    _delete_the_file(app, storage_client)
+
+
+def _upload_the_file(app: Flask, storage_client: Minio) -> None:
+    with app.app_context():
+        assert heartbeat()
+
+    if not storage_client.bucket_exists(BUCKET_NAME):
+        storage_client.make_bucket(BUCKET_NAME)
+    
+    with NamedTemporaryFile() as file:
+        file.write(FILE_STRING.encode("utf-8"))
+        file.seek(0)
+        assert file.read() == FILE_STRING.encode("utf-8")
+        storage_client.fput_object(BUCKET_NAME, FILE_NAME, file.name)
+
+
+def _delete_the_file(app: Flask, storage_client: Minio) -> None:
+    with app.app_context():
+        assert heartbeat()
+
+    if not storage_client.bucket_exists(BUCKET_NAME):
+        return
+
+    storage_client.remove_object(BUCKET_NAME, FILE_NAME)
+    
+    with raises(Exception):
+        storage_client.stat_object(BUCKET_NAME, FILE_NAME)
+
+
+class TestMinIOStorage:
+    def test_heartbeat_should_return_true(self, app: Flask):
+        with app.app_context():
+            
+            assert heartbeat()
+
+    def test_heartbeat_with_wrong_secret_should_return_false(self, app: Flask, monkeypatch: MonkeyPatch):
+        modified_environ: dict[str, str] = os.environ.copy()
+        modified_environ["MINIO_SECRET_KEY"] = "WRONG_SECRET_KEY"
+        monkeypatch.setattr(storage.minio_util, "environ", modified_environ)
+        with app.app_context():
+
+            assert not heartbeat()
+
+    def test_download_file_with_no_heart_beat_should_revert_the_action(self, app: Flask, monkeypatch: MonkeyPatch):
+        modified_environ: dict[str, str] = os.environ.copy()
+        modified_environ["MINIO_SECRET_KEY"] = "WRONG_SECRET_KEY"
+        monkeypatch.setattr(storage.minio_util, "environ", modified_environ)
+        with app.app_context():
+            with TemporaryDirectory() as temp_dir:
+
+                with raises(Exception):
+                    download(BUCKET_NAME, FILE_NAME, Path(temp_dir) / FILE_NAME)    
+
+    def test_download_file_from_storage_server_should_got_the_file(self, app: Flask, storage_client: Minio, place_the_file: None):
+        with app.app_context():
+            with TemporaryDirectory() as temp_dir:
+
+                download(BUCKET_NAME, FILE_NAME, Path(temp_dir) / FILE_NAME)
+
+                with open(Path(temp_dir) / FILE_NAME, "r") as file:
+                    assert file.read() == FILE_STRING
