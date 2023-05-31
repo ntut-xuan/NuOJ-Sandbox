98a04d51b63e696a51eef59f509040fe51d00af3
ntut-xuan <t109590031@ntut.org.tw>
2023-05-31 06:25:55 +0000

    Complete the download function and testcase

--- backend/storage/minio_util.py
+++ backend/storage/minio_util.py
@@ -2,20 +2,27 @@ from os import environ
 from traceback import format_exc
 from typing import Final
 
-from minio import Minio
 from flask import current_app
+from loguru import logger
+from minio import Minio
 
 from setting.util import Setting
 
 
-def heartbeat() -> bool:
+def get_client() -> Minio:
     setting: Setting = current_app.config["setting"]
-    
+
     ACCESS_KEY: Final[str] = environ.get("MINIO_ACCESS_KEY")
     SECRET_KEY: Final[str] = environ.get("MINIO_SECRET_KEY")
     ENDPOINT: Final[str] = setting.minio.endpoint
 
     client = Minio(endpoint=ENDPOINT, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
+
+    return client
+
+
+def heartbeat() -> bool:
+    client: Minio = get_client()
 
     try:
         client.bucket_exists("notexistbucket")
@@ -23,3 +30,13 @@ def heartbeat() -> bool:
     except:
         print(format_exc())
         return False
+
+
+def download(bucket_name: str, object_name: str, file_name: str) -> None:
+    assert heartbeat()
+    
+    client: Minio = get_client()
+
+    client.fget_object(bucket_name, object_name, file_name)
+
+    logger.info(f"Fetch the file {object_name} from {bucket_name}.")
